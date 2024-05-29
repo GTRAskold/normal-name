edecf2adddb6a4f1928116b637109b1e125a3bcd
Cybersport <Cybersport@26kadr.internal>
2024-05-29 15:51:53 +0300

    7

---
+++ script.js
@@ -0,0 +1,34 @@
+// задание1
+// let hah = document.getElementById("jocker")
+// hah.innerHTML = "скидыщ"
+
+// задание2
+
+// let hah2 = document.getElementById("arp")
+// hah2.setAttribute("class", "arp")
+
+// задание 3
+
+// let hah3 = document.getElementById("bee")
+// hah3.style.color = "blue"
+
+// задание 4
+
+// let hah4 = document.getElementById("jocker");
+// console.log(hah2)
+
+// задание 5
+
+// let div = document.getElementById('buu')
+// let element = document.createElement('li');
+// element.className="but"
+// element.innerHTML="+"
+// div.append(element)
+
+// задание 6
+
+// function addElement() {
+    
+//     var header = document.getElementById("hide");
+//     header.hidden = true
+// }
